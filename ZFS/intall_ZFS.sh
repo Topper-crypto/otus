28863a230893f454bf6733d4dd7a6ffe6993f2e7
ANATOLY KOROVIN <74492568+Topper-crypto@users.noreply.github.com>
2022-05-24 02:13:24 +0300

    Create intall_ZFS.sh

---
+++ ZFS/intall_ZFS.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo -i
+sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
+sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+yum install -y yum-utils
+yum install -y https://zfsonlinux.org/epel/zfs-release.el8_5.noarch.rpm
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-zfsonlinux
+yum install -y epel-release kernel-devel zfs
+yum-config-manager --disable zfs
+yum-config-manager --enable zfs-kmod
+yum install -y zfs
+modprobe zfs
+yum install -y wget
