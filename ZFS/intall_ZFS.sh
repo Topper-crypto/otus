695cfdf183df10e421e87979494d64a9d1b3e844
ANATOLY KOROVIN <74492568+Topper-crypto@users.noreply.github.com>
2022-05-24 02:22:29 +0300

    Update intall_ZFS.sh

--- ZFS/intall_ZFS.sh
+++ ZFS/intall_ZFS.sh
@@ -2,6 +2,7 @@
 sudo -i
 sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+yum update -y
 yum install -y yum-utils
 yum install -y https://zfsonlinux.org/epel/zfs-release.el8_5.noarch.rpm
 rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-zfsonlinux
