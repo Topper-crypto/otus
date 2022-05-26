05539b00a1b160d682f03734b9f459ffaf9974c2
ANATOLY KOROVIN <74492568+Topper-crypto@users.noreply.github.com>
2022-05-26 20:48:15 +0300

    Update nfsc_script.sh

--- NFS, FUSE/nfsc_script.sh
+++ NFS, FUSE/nfsc_script.sh
@@ -2,5 +2,5 @@
 yum update -y
 yum install nfs-utils -y
 mkdir /var/share
-echo "10.1.1.10:/var/share /var/share nfs proto=udp,timeo=5,retrans=3,rsize=8192,wsize=8192 0 0" >> /etc/fstab
+echo "192.168.50.11:/var/share /var/share nfs proto=udp,timeo=5,retrans=3,rsize=8192,wsize=8192 0 0" >> /etc/fstab
 mount -a
