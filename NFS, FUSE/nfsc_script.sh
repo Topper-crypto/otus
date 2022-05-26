403485d35914f143dac51bfd90f408d4744017a8
ANATOLY KOROVIN <74492568+Topper-crypto@users.noreply.github.com>
2022-05-26 23:02:27 +0300

    Update nfsc_script.sh

--- NFS, FUSE/nfsc_script.sh
+++ NFS, FUSE/nfsc_script.sh
@@ -2,9 +2,12 @@
 vagrant ssh nfsc
 yum update -y
 yum install nfs-utils -y
+systemctl start rpcbind
+systemctl enable rpcbind
 systemctl enable firewalld --now
 systemctl status firewalld
-echo "192.168.50.10:/srv/share/ /mnt nfs vers=3,proto=udp,noauto,xsystemd.automount 0 0" >> /etc/fstab
+systemctl enable nfs --now
+echo "192.168.50.10:/srv/share/ /mnt nfs vers=3,proto=udp,noauto,x-systemd.automount 0 0" >> /etc/fstab
 systemctl daemon-reload
 systemctl restart remote-fs.target
 mount | grep mnt
