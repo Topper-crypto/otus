ec0bbc416797c8b7a29d3f0ed5981bd19c08c053
ANATOLY KOROVIN <74492568+Topper-crypto@users.noreply.github.com>
2022-05-26 21:48:54 +0300

    Update nfsc_script.sh

--- NFS, FUSE/nfsc_script.sh
+++ NFS, FUSE/nfsc_script.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+vagrant ssh nfsc
 yum update -y
 yum install nfs-utils -y
-mkdir /var/share
-echo "192.168.50.11:/var/share /var/share nfs proto=udp,timeo=5,retrans=3,rsize=8192,wsize=8192 0 0" >> /etc/fstab
-mount -a
+systemctl enable firewalld --now
+systemctl status firewalld
+echo "192.168.50.10:/srv/share/ /mnt nfs vers=3,proto=udp,noauto,xsystemd.automount 0 0" >> /etc/fstab
+systemctl daemon-reload
+systemctl restart remote-fs.target
+mount | grep mnt
