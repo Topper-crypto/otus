97a8dba5825f704abd5037850cc910620dfd49b9
ANATOLY KOROVIN <74492568+Topper-crypto@users.noreply.github.com>
2022-06-02 23:40:59 +0300

    Create test.sh

---
+++ boot/test.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cat <<'msgend'
+Hello! You are in dracut module!
+ _________________________________________
+< I'm dracut module from otus. Korovin A. >
+ -----------------------------------------
+   \
+    \
+    /\_____/\
+   /  o   o  \
+  ( ==  ^  == )
+   )         (
+  (           )
+ ( (  )   (  ) )
+(__(__)___(__)__)
+msgend
+sleep 10
+echo " continuing...."
