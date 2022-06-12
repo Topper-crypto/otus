3d9242c38b649c6a64e05f521c9e50e7c0b21526
ANATOLY KOROVIN <74492568+Topper-crypto@users.noreply.github.com>
2022-06-12 19:36:33 +0300

    Update provision.sh

--- Bash/provision.sh
+++ Bash/provision.sh
@@ -3,10 +3,8 @@ yum update -y
 yum install -y mailx flock
 chmod +x /vagrant/pars.sh
 
-#Начальная дата для певого фильтра лога, для демонстрации работы 
 cat >> /var/tmp/parstime.log << EOF
-1990-01-01T00:00:00
+2019-01-01T00:00:00
 EOF
 
-#Задания с блокировкой повторного запуска через flock
 crontab -l | { cat; echo "0 */1 * * * /usr/bin/flock -xn /var/lock/pars.lock -c 'sh /vagrant/pars.sh'"; } | crontab - 
